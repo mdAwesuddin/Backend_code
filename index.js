992e37836406aa170d89e6cd26d256e371f412e3
Awesuddin <awesuddin@edvenswatech.com>
2024-05-26 15:11:44 +0530

    14th commit

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ import AppExpress from '@itznotabug/appexpress';
 import router from './src/router.js';
 
 
-const app =new AppExpress();
-app.use(AppExpress.json());
+const app = new AppExpress();
+app.use((req, res, next) => {
+    let data = '';
+    req.on('data', chunk => {
+        data += chunk;
+    });
+    req.on('end', () => {
+        try {
+            req.body = JSON.parse(data);
+            next();
+        } catch (error) {
+            res.status(400).json({ error: 'Invalid JSON' });
+        }
+    });
+});
 app.use('/users', router);
 
 const getRoutes = (request, response) => {
