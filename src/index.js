fc917a97c9fab8efe0357e25e9e3dea1516957a6
Awesuddin <awesuddin@edvenswatech.com>
2024-05-26 17:55:35 +0530

    29th commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import express from 'express';
 import dotenv from 'dotenv'
 
 dotenv.config();
-const app2 =new AppExpress();
 const app = express();
 app.use(express.json());
 app.use('/users', router);
